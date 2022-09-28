7b1fef678428b8f64105dc7ea3c4ded25abd7915
AnnFlets <analuciaflets@gmail.com>
2022-09-28 17:32:46 -0600

    commit rama ana

--- Kapa/src/kapa/Kapa.java
+++ Kapa/src/kapa/Kapa.java
@@ -5,5 +5,6 @@ public class Kapa {
     public static void main(String[] args) {
         System.out.println("KAPA");
         System.out.println("Hola");
+        System.out.println("KAPA");
     }
 }
